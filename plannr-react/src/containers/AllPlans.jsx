81079916019e2d371c0fb5f3044ae1b63ee47b21
porzingod <jasonlim.ny@gmail.com>
2018-04-13 15:08:58 -0400

    join button shows for plans you are not part of

--- plannr-react/src/containers/AllPlans.jsx
+++ plannr-react/src/containers/AllPlans.jsx
@@ -12,27 +12,17 @@ export default class AllPlans extends React.Component {
 
     let notJoined = []
     if (nextProps.allPlans.length > 0 && nextProps.myPlans.length > 0) {
-      debugger
-       nextProps.allPlans.forEach(plan => {
-         debugger
-         if(!nextProps.myPlans.includes(plan)) {
-
-           notJoined.push(plan)
-
+       nextProps.allPlans.map( plan => plan.id).forEach( planId => {
+         if(!nextProps.myPlans.map( plan => plan.id ).includes(planId)) {
+           notJoined.push(nextProps.allPlans[planId - 1])
          }
       })
     }
     this.setState({notJoinedPlans: notJoined})
-
   }
 
-
-
   renderPlans = () => {
-    return this.props.allPlans.map( plan =>
-      this.state.notJoinedPlans.includes(plan) ?
-     <Plan key={plan.id} plan={plan} notJoined={true} />
-     : <Plan key={plan.id} plan={plan} notJoined={false} />)
+    return this.props.allPlans.map( plan => this.state.notJoinedPlans.includes(plan) ? <Plan key={plan.id} plan={plan} notJoined={true} /> : <Plan key={plan.id} plan={plan} notJoined={false} /> )
   }
 
   render() {
